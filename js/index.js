b48ac9931e84feda7db849fb848bde1a80951e2b
bek-211203-coder <bek406197@gmail.com>
2024-06-09 21:04:07 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,127 @@
+// break/continue ga oid masalalar
+// masala 1
+// for (let i = 1; i<=50; i++){
+//     if (i==30){
+//         // continue;
+//         break;
+//     }
+//     console.log(i)
+// }
+
+// masala 2
+// for (let i = 1; i<=50; i++){
+//     if (i%5==0){
+//         continue;
+//         // break;
+//     }
+//     console.log(i)
+// }
+
+// masala 3
+// for (let i = 1; i<=100; i++){
+//     if (i==75){
+//         // continue;
+//         break;
+//     }
+//     console.log(i)
+// }
+
+// masala 4
+// for (let i = 1; i<=30; i++){
+//     if (i%3==0){
+//         continue;
+//         // break;
+//     }
+//     console.log(i)
+// }
+
+
+
+// While hamda do…while yordamida ishlanadigan masalalar
+
+// masala 1
+// let i = 1;
+// while(i<=20){
+//     console.log(i)
+//     i++
+// }
+
+// masala 2
+// let i = 10;
+// while(i>=1){
+//     console.log(i)
+//     i--
+// }
+
+// masala 3
+// let i = 2;
+// while(i<=50){
+//     console.log(i)
+//     i+=2
+// }
+
+// masala 4
+// let i = 1;
+// while(i<=50){
+//     console.log(i)
+//     i+=2
+// }
+
+// masala 5
+// let i = 1;
+// let sum = 0;
+// while(i<=100){
+//     sum+=i
+//     i++
+// }
+// console.log(sum);
+
+// masala 6
+// let i = 1;
+// while(i<=10){
+//     console.log(i**2);
+//     i++
+// }
+
+// masala 7
+// -------------
+// masala 8
+// let i = 1;
+// while(i<=1024){
+//     console.log(i);
+//     i*=2
+// }
+
+// masala 10
+// let i = 1;
+// while(i<=20){
+//     if ( i % 3==0){
+//         console.log(i)
+//     }
+//     i++
+// }
+
+// do...while` sikl operatoriga oid masalalar:
+
+// masala 1
+// let i = 1;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 10);
+
+// masala 2 
+// let i = 20;
+// do {
+//   console.log(i);
+//   i--;
+// } while (i >= 1);
+
+// masala 3
+// let i = 2;
+// do {
+//   console.log(i);
+//   i+=2;
+// } while (i <= 100);
+
+
